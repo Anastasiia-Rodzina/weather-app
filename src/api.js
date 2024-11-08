6fbe88354823e11d6a23442845de16ea2727c7d6
Anastasiia-Rodzina <pototzkaja.n@gmail.com>
2024-11-08 14:24:00 +0200

    input with autocomplete, fetch cities

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7199e988damsh9fdf5dde4af5625p16871cjsn511d7a07c495",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "28e57fc63252ef6948be040354a0be71";
